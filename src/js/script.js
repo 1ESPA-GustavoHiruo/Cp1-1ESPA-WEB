243250943771d878c4502cbfe3e4924bf5bb1135
Gustavo <Gustavohiruo@gmail.com>
2025-10-09 15:31:31 -0300

    Resolvendo o exercício 2

--- src/js/script.js
+++ src/js/script.js
@@ -9,5 +9,28 @@ let vazio = null;
 console.log(vazio);
 
 // Variável com valor de JavaScript
-let java = JavaScript;
+let java = "JavaScript";
 console.log(java)
+
+
+// Exercicio 2, Como usar o operador 
+
+const a = 10
+const b = 50
+const c = 10
+
+// != Remete a diferente de ...
+console.log(a != b)     // true 
+console.log(a != c)     // false
+
+// == sinal de igualdade 
+console.log(a == c)     //true
+console.log(a == b)     //false
+
+// >= sinal de maior ou igual a ...
+console.log(b >= a)     //true
+console.log(a >= b)     //false
+
+
+
+
