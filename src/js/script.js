b13af9ad39b161a62b5cde0f2a913394a9d304fe
Gustavo <Gustavohiruo@gmail.com>
2025-10-09 15:16:03 -0300

    Exercicio 1 Declarando variável

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// Exercício 1, Como declarar uma variável
+
+// Variável com valor undefined
+let indefinido;
+console.log(indefinido);
+
+// Valor vazio
+let vazio = null;
+console.log(vazio);
+
+// Variável com valor de JavaScript
+let java = JavaScript;
+console.log(java)
